a0e852deb55f27b44d9df100ab13c1867e563e78
Julien Vehent <julien@linuxwall.info>
2015-01-12 22:10:17 +0000

    disable disabled users

--- manifests/site.pp
+++ manifests/site.pp
@@ -7,12 +7,16 @@ node default {
     create_resources('account::user', $accounts)
 
     # delete disabled users
-    $disabled = hiera_hash('disabled_users')
-    create_resources('account::user', $disabled)
+    # $disabled = hiera_hash('disabled_users')
+    # create_resources('account::user', $disabled)
 }
 
 node 'ip-172-19-1-46.ec2.internal' {
     include puppet::master
+}
+
+node 'ip-172-19-1-83.ec2.internal' {
+    include os_hardening
 }
 
 # https://ask.puppetlabs.com/question/6640/warning-the-package-types-allow_virtual-parameter-will-be-changing-its-default-value-from-false-to-true-in-a-future-release/
