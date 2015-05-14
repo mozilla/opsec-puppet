a68ae768e9ebc4334bdbf8aff063eaa32c18ba30
Julien Vehent <julien@linuxwall.info>
2015-05-14 11:38:14 -0400

    provision compliance-item mig worker

--- modules/mig/manifests/server/compliance_item_worker.pp
+++ modules/mig/manifests/server/compliance_item_worker.pp
@@ -1,7 +1,7 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
-class mig::server::agent_intel_worker(
+class mig::server::compliance_item_worker(
 
 ){
     case $::operatingsystem {
@@ -9,27 +9,27 @@ class mig::server::agent_intel_worker(
             include mig::server::base
             include mig::server::baseworker
             file {
-                '/etc/mig/agent-intel-worker.cfg':
-                    content     => template('mig/agent-intel-worker.cfg.erb'),
+                '/etc/mig/compliance-item-worker.cfg':
+                    content     => template('mig/compliance-item-worker.cfg.erb'),
                     show_diff   => false,
                     owner       => 'mig',
                     mode        => 600,
                     require     => [ Class['mig::server::base'], Class['mig::server::baseworker'] ];
             }
             exec {
-                'set-aiworker-db-password':
-                    command     => 'sed -i "s|REPLACEMOZDEFPASSWORD|$(cat /etc/mig/worker-mozdef-password)|" /etc/mig/agent-intel-worker.cfg',
+                'set-ciworker-db-password':
+                    command     => 'sed -i "s|REPLACEMOZDEFPASSWORD|$(cat /etc/mig/worker-mozdef-password)|" /etc/mig/compliance-item-worker.cfg',
                     path        => "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin",
-                    require     => [ File['/etc/mig/agent-intel-worker.cfg']];
-                'set-aiworker-mq-password':
-                    command     => 'sed -i "s|REPLACEMQPASSWORD|$(cat /etc/mig/worker-mq-password)|" /etc/mig/agent-intel-worker.cfg',
+                    require     => [ File['/etc/mig/compliance-item-worker.cfg']];
+                'set-ciworker-mq-password':
+                    command     => 'sed -i "s|REPLACEMQPASSWORD|$(cat /etc/mig/worker-mq-password)|" /etc/mig/compliance-item-worker.cfg',
                     path        => "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin",
-                    require     => [ File['/etc/mig/agent-intel-worker.cfg']];
+                    require     => [ File['/etc/mig/compliance-item-worker.cfg']];
             }
             service {
-                'mig-agent-intel-worker':
+                'mig-compliance-item-worker':
                     enable => true,
-                    require => [ File['/etc/mig/agent-intel-worker.cfg'] ];
+                    require => [ File['/etc/mig/compliance-item-worker.cfg'] ];
             }
         }
     }
